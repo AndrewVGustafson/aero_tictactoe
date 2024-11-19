aa252f73e6b81541ae3d598c03eee3e39e93f847
Andrew G <f4andrew7@gmail.com>
2024-11-19 00:19:26 -0600

    added semifunctional window

--- main.py
+++ main.py
@@ -16,18 +16,7 @@ def draw_text(screen, text, color, x, y, font=None):
     image = font.render(text, True, color)
     screen.blit(image, (x, y))
 
-
-def main():
-    pg.init()
-    pg.rect.Rect.center
-    pg.display.set_caption("Tic-Tac-Toe")
-    screen = pg.display.set_mode((800, 800))
-    
-    menu = pygame_menu.Menu("test", 800, 800, pygame_menu.themes.THEME_BLUE)
-    menu.add.text_input('', default='John Doe')
-    menu.add.button("aaaaaaa", lambda: print("a"))
-    menu.mainloop(screen)
-
+def start_game() -> None:
     game = TicTacToe()
     game.start_game()
 
@@ -38,6 +27,20 @@ def main():
 
         pg.display.update()
 
+
+def main():
+    pg.init()
+    pg.rect.Rect.center
+    pg.display.set_caption("Tic-Tac-Toe")
+    
+    menu = pygame_menu.Menu("test", 800, 800, theme=pygame_menu.themes.THEME_BLUE)
+    menu.add.button("Start", start_game)
+    menu.mainloop(screen)
+
+    while True:
+        pg.display.update()
+
             
 if __name__ == "__main__":
+    screen = pg.display.set_mode((800, 800))
     main()
